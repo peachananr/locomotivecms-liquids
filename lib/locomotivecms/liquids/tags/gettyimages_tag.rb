8d546488ec3141cc2f070794fb52bc50b8b0bcba
Pete R <peachananr@gmail.com>
2018-12-21 18:41:05 +0700

    Fix getty

--- lib/locomotivecms/liquids/tags/gettyimages_tag.rb
+++ lib/locomotivecms/liquids/tags/gettyimages_tag.rb
@@ -24,16 +24,28 @@ module LocomotiveCMS
 
             # create instance of the SDK
             apiClient = ApiClient.new(api_key, api_secret)
-            return result = apiClient
-                .search_images()
-                .with_phrase(terms)
-                .with_graphical_styles(["photography"])
-                .with_fields(["referral_destinations", "preview", "title", "id"])
-                .with_exclude_nudity("true")
-                .with_page(page)
-                .with_page_size(size)
-                .execute()
+            begin
+              return result = apiClient
+                  .search_images()
+                  .with_phrase(terms)
+                  .with_graphical_styles(["photography"])
+                  .with_fields(["referral_destinations", "preview", "title", "id"])
+                  .with_exclude_nudity("true")
+                  .with_page(page)
+                  .with_page_size(size)
+                  .execute()
 
+            rescue => error
+              return result = apiClient
+                  .search_images()
+                  .with_phrase(terms)
+                  .with_graphical_styles(["photography"])
+                  .with_fields(["referral_destinations", "preview", "title", "id"])
+                  .with_exclude_nudity("true")
+                  .with_page(1)
+                  .with_page_size(6)
+                  .execute()
+            end
           end
       end
     end
