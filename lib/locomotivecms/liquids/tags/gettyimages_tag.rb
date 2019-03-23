58f84f89e23dde8581c547d255d0872f7b686c93
Pete R <peachananr@gmail.com>
2019-03-23 10:09:10 +0700

    New sort order

--- lib/locomotivecms/liquids/tags/gettyimages_tag.rb
+++ lib/locomotivecms/liquids/tags/gettyimages_tag.rb
@@ -33,7 +33,7 @@ module LocomotiveCMS
                   .with_exclude_nudity("true")
                   .with_page(page.to_i)
                   .with_page_size(size.to_i)
-                  .with_sort_order(sort_order)
+                  .with_sort_order("#{sort_order}")
                   .execute()
 
             rescue => error
@@ -45,7 +45,7 @@ module LocomotiveCMS
                   .with_exclude_nudity("true")
                   .with_page(1)
                   .with_page_size(6)
-                  .with_sort_order(sort_order)
+                  .with_sort_order("#{sort_order}")
                   .execute()
             end
           end
