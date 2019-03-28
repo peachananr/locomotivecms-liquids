397ff9341ef57d1b5fce233f72f0621d6c502023
Pete R <peachananr@gmail.com>
2019-03-28 15:57:48 +0700

    Add timeout

--- lib/locomotivecms/liquids/tags/gettyimages_tag.rb
+++ lib/locomotivecms/liquids/tags/gettyimages_tag.rb
@@ -18,6 +18,7 @@ module LocomotiveCMS
 
           def init_gi(terms, page, size, sort_order)
             require 'gettyimages-api'
+            require 'timeout'
 
             api_key = ENV['GETTY_KEY']
             api_secret = ENV['GETTY_SECRET']
@@ -25,28 +26,22 @@ module LocomotiveCMS
             # create instance of the SDK
             apiClient = ApiClient.new(api_key, api_secret)
             begin
-              return result = apiClient
-                  .search_images()
-                  .with_phrase("#{terms}")
-                  .with_graphical_styles(["photography"])
-                  .with_fields(["referral_destinations", "preview", "title", "id"])
-                  .with_exclude_nudity("true")
-                  .with_page(page.to_i)
-                  .with_page_size(size.to_i)
-                  .with_sort_order("#{sort_order}")
-                  .execute()
+              Timeout::timeout(5) {
+                return result = apiClient
+                    .search_images()
+                    .with_phrase("#{terms}")
+                    .with_graphical_styles(["photography"])
+                    .with_fields(["referral_destinations", "preview", "title", "id"])
+                    .with_exclude_nudity("true")
+                    .with_page(page.to_i)
+                    .with_page_size(size.to_i)
+                    .with_sort_order("#{sort_order}")
+                    .execute()
+              }
+
 
             rescue => error
-              return result = apiClient
-                  .search_images()
-                  .with_phrase("#{terms}")
-                  .with_graphical_styles(["photography"])
-                  .with_fields(["referral_destinations", "preview", "title", "id"])
-                  .with_exclude_nudity("true")
-                  .with_page(1)
-                  .with_page_size(6)
-                  .with_sort_order("most_popular")
-                  .execute()
+              return ""
             end
           end
       end
