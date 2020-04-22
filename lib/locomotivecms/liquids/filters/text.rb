1beed32ce9a3eda1e8bd16b35aa24549b01aadde
Pete R <peachananr@gmail.com>
2020-04-22 11:45:10 +0700

    Remove ads on first few para 3

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -87,20 +87,22 @@ module LocomotiveCMS
         def remove_placeholder_img(input)
           require 'nokogiri'
           html = Nokogiri.HTML(input)
+          if html.css('.table-of-contents-wrapper').size > 0
+            html.css('.table-of-contents-wrapper').first.inner_html = "#{html.css('.table-of-contents-wrapper').first.inner_html}xxx"
+            string = html.css('body').first.to_s
+            string.gsub!("<body>", "<body><div>")
+            string.gsub!("</div>xxx", "</div></div>")
 
-          if html.css('#table-of-contents').size > 0
-            wrap = html.xpath('//div[@id="table-of-contents"]/preceding::p')
-            wrap = "<div>#{wrap}</div>"
-            html.xpath('//div[@id="table-of-contents"]/preceding::p').remove
-            html.css(".table-of-contents-wrapper").first.parent.inner_html = "#{wrap}#{html.css(".table-of-contents-wrapper").first.parent.inner_html}"
-          else
-            if html.css('body > h3').size > 0
-              wrap = html.xpath('(//h3)[1]/preceding::p')
-              wrap = "<div>#{wrap}</div>"
-              html.xpath('(//h3)[1]/preceding::p').remove
-              html.css("body > h3").first.parent.inner_html = "#{wrap}#{html.css("body > h3").first.parent.inner_html}"
-            end
+            html = Nokogiri.HTML(string)
           end
+          #if html.css('.table-of-contents-wrapper').size > 0
+            #html.css("body").first.inner_html = "<div>#{html.css("body").first.inner_html}"
+            #html.css(".table-of-contents-wrapper").first.inner_html = "#{html.css(".table-of-contents-wrapper").first.inner_html}</div>"
+            #wrap = html.xpath('//div[@id="table-of-contents"]/preceding::p')
+            #wrap = "<div>#{wrap}</div>"
+            #html.xpath('//div[@id="table-of-contents"]/preceding::p').remove
+            #html.css(".table-of-contents-wrapper").first.parent.inner_html = "#{wrap}#{html.css(".table-of-contents-wrapper").first.parent.inner_html}"
+          #end
 
           html.css('img.lazy').each do |i|
 
