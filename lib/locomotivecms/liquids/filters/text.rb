d873f919c4f328293aa8b4336913270306b15b1a
Pete Rojwongsuriya <peterojwongsuriya@Petes-MacBook-Pro.local>
2022-03-20 08:39:09 +0700

    Fix more

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -140,7 +140,7 @@ module LocomotiveCMS
           end
 
           if html.css('.table-of-contents-wrapper').size > 0
-            html.css('.table-of-contents-wrapper').first.inner_html = "#{html.css('.#table-of-contents-wrapper').first.inner_html}-xxx"
+            html.css('.table-of-contents-wrapper').first.inner_html = "#{html.css('.table-of-contents-wrapper').first.inner_html}-xxx"
             string = html.css('body').first.to_s
             string.gsub!("<body>", "<body><div>")
             string.gsub!("-xxx</div>", "</div></div>")
@@ -201,7 +201,7 @@ module LocomotiveCMS
           end
 
           if html.css('.table-of-contents-wrapper').size > 0
-            html.css('.table-of-contents-wrapper').first.inner_html = "#{html.css('.#table-of-contents-wrapper').first.inner_html}-xxx"
+            html.css('.table-of-contents-wrapper').first.inner_html = "#{html.css('.table-of-contents-wrapper').first.inner_html}-xxx"
             string = html.css('body').first.to_s
             string.gsub!("<body>", "<body><div>")
             string.gsub!("-xxx</div>", "</div></div>")
