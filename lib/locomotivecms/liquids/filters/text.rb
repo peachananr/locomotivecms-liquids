dd766ce076563324e8b520b604dd7b72340e9c99
Pete Rojwongsuriya <peterojwongsuriya@Petes-MacBook-Pro.local>
2022-05-17 08:35:55 +0700

    Fix more

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -12,7 +12,7 @@ module LocomotiveCMS
         end
 
         def utf_encode(input)
-          input.encode('iso-8859-1').encode('UTF-8')
+          input.force_encoding('iso-8859-1').encode("utf-8")
         end
 
         def capitalize_all(input)
