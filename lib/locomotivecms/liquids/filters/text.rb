384fa473157e5f3dc0e8220ab12dd7c1bcd87c63
Pete Rojwongsuriya <peterojwongsuriya@Petes-MacBook-Pro.local>
2023-08-02 10:16:29 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -293,7 +293,7 @@ module LocomotiveCMS
                   selected_ps[1].add_next_sibling(pinterest)
                 end
 
-              elsif !selected_ps.nil? and !selected_ps[1].nil?
+              elsif !selected_ps2.nil? and !selected_ps2[1].nil?
                 unless !selected_ps2[1].next_element.nil? and !selected_ps2[1].next_element["class"].nil? and selected_p2s[1].next_element["class"].include? "readmore-block"
                   pinterest = "<div class=\"pin-it-section\" id=\"pinterest\">#{html.at_css("#pinterest").inner_html}</div>"
                   html.at_css("#pinterest").remove()
