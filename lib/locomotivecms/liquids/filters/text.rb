a0440810d9f7c39f4853dfacb5a23e48ddf50f97
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.33>
2023-08-02 14:51:06 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -291,7 +291,7 @@ module LocomotiveCMS
             
             if target_p
 
-              while target_p && (target_p.next_element&.name == 'p') && (target_p.next_element&.classes&.any? { |cls| cls.include?('block') }) and (target_p.previous_element.name != "h2" or target_p.previous_element.name != "h3")
+              while target_p && ((target_p.next_element.name == 'p' || target_p.next_element.css('img').any?)) && (target_p.next_element&.classes&.any? { |cls| cls.include?('block') })
                 puts "yy"
                 target_p = target_p.next_element
               end
