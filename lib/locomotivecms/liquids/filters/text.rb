bb35aae6cb0bbf12ed653ee4fd4822889a476484
Pete Rojwongsuriya <peterojwongsuriya@Petes-MacBook-Pro.local>
2023-08-02 09:52:11 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -281,10 +281,10 @@ module LocomotiveCMS
           end
 
           if html.css('#pinterest').size > 0 and html.css("h3").size > 2
-            if html.css("h2:eq(3) ~ p:not(:empty):not(:has(img))").size > 0 or html.css("h3:eq(3) ~ p:not(:empty):not(:has(img))").size > 0
+            if html.css("h2:eq(3) ~ p:not(:empty):not(:has(img))").size > 0 or html.css("h3:eq(3) ~ p:not(:empty):not(:has(img))").size > 0 and (!html.css("h3:eq(1) ~ p:not(:empty):not(:has(img))")[1]["class"].nil? and !html.css("h3:eq(1) ~ p:not(:empty):not(:has(img))")[1]["class"].include? "readmore")
               pinterest = "<div class=\"pin-it-section\" id=\"pinterest\">#{html.at_css("#pinterest").inner_html}</div>"
               html.at_css("#pinterest").remove()
-              if !html.css("h3:eq(2) ~ p:not(:empty):not(:has(img))").nil? and !html.css("h3:eq(2) ~ p:not(:empty):not(:has(img))")[1].nil?
+              if !html.css("h3:eq(2) ~ p:not(:empty):not(:has(img))").nil? and !html.css("h3:eq(2) ~ p:not(:empty):not(:has(img))")[1].nil?  and (!html.css("h3:eq(1) ~ p:not(:empty):not(:has(img))")[1]["class"].nil? and !html.css("h3:eq(1) ~ p:not(:empty):not(:has(img))")[1]["class"].include? "readmore")
                 html.css("h3:eq(2) ~ p:not(:empty):not(:has(img))")[1].add_next_sibling(pinterest)
               else
                 html.css("h2:eq(2) ~ p:not(:empty):not(:has(img))")[1].add_next_sibling(pinterest)
