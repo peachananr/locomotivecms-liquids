f4f3ccacd8866df7686e3dc267a5a925e6ef4117
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.33>
2023-08-03 09:26:23 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -302,14 +302,14 @@ module LocomotiveCMS
             h2 = el.at_css('h2')["id"]
 
             if html.css('.activity-block').size == 1
-              block = html.css('.activity-block h2')["id"]
+              block = html.at_css('.activity-block h2')["id"]
               html.at_css('.activity-block').add_next_sibling(el)
               el2 = html.at_css(".toc-list .toc-l1 a[href*='#{block.downcase}']")
               puts "xxx #{el2}"
               html.at_css(".toc-list .toc-l1 a[href*='#{h2.downcase}']").parent.add_next_sibling(el2.parent)
               #el.remove
             elsif html.css('.video-block-wrapper').size == 1
-              block = html.css('.activity-block h2')["id"]
+              block = html.at_css('.activity-block h2')["id"]
               html.at_css('.video-block-wrapper').add_next_sibling(el)
               el2 = html.at_css(".toc-list .toc-l1 a[href*='#{block.downcase}']")
               html.at_css(".toc-list .toc-l1 a[href*='#{h2.downcase}']").parent.add_next_sibling(el2.parent)
