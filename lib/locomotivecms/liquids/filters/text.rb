09ded2bbe77a81f13f2c579ffec414bb8a1499e1
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.35>
2023-09-18 06:11:58 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -246,7 +246,7 @@ module LocomotiveCMS
                 # Iterate through the h2 elements
                 h2_elements.each do |h2|
                   # Check if the h2 text starts with a number
-                  if h2.text.strip.match?(/^(?![0-9])(?!.*\bmap\b)(?=.*(?:things to do|best places to)).*$/i)
+                  if h2.text.strip.match?(/^\d/i) or h2.text.strip.match?(/^(?![0-9])(?!.*\bmap\b)(?=.*(?:things to do|best places to)).*$/i)
                     # Find adjacent h3 elements until the next h2 is encountered
                     next_element = h2.next_element
                     while next_element && next_element.name != 'h2'
