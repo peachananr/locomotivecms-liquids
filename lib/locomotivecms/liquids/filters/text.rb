99f482ca587df9defae8acc8b570256189d8d10d
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.35>
2023-09-17 18:52:42 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -142,6 +142,78 @@ module LocomotiveCMS
           return tags
         end
 
+        def amp_story(input, slug = '') 
+          require 'nokogiri'
+          html = Nokogiri.HTML(input)
+          result = []
+
+          if html.css('h2').size > 0
+            # Adding numbering on h3
+            h2_elements = html.css('h2')
+
+            # Initialize a counter for the h3 elements
+            h3_counter = 1
+
+            # Iterate through the h2 elements
+            h2_elements.each do |h2|
+              # Check if the h2 text starts with a number
+              if h2.text.strip.match?(/^\d(?!.*itinerary)/i) or h2.text.strip.match?(/^(?![0-9])(?!.*\bmap\b)(?=.*(?:things to do|best places to)).*$/i)
+                # Find adjacent h3 elements until the next h2 is encountered
+                next_element = h2.next_element
+                while next_element && next_element.name != 'h2'
+                  break if h3_counter == 5;
+
+                  if next_element.name == 'h3'
+                    # Add the number counter to the h3 element's text
+                    if !next_element.text.match?(/^\d+\./)
+                      img = ""
+                      link = ""
+                      if !slug.nil?
+                        link = '<amp-story-page-outlink layout="nodisplay">
+                        <a href="https://www.bucketlistly.blog/posts/#{slug}" title="Go to Blog">Go to Blog</a>
+                        </amp-story-page-outlink>'
+                      end
+                      content = "<amp-story-grid-layer template=\"vertical\" class=\"vertical_full\">
+                      <div class=\"title safe_area\">
+                    <h2>{{#{h3_counter}. #{next_element.inner_html.strip}}}</h2>
+                              </div>
+                              <svg xmlns=\"http://www.w3.org/2000/svg\" class=\"mask\" xml:space=\"preserve\" fill-rule=\"evenodd\" stroke-linejoin=\"round\" stroke-miterlimit=\"2\" clip-rule=\"evenodd\" viewBox=\"0 0 1183 43\"><path fill=\"#f8f3f3\" fill-rule=\"nonzero\" d=\"M1183 42S648-36 0 42V0h1183v42Z\"/></svg>
+                              <div  class=\"logo\">
+                                <svg width=\"35\" height=\"35\" viewBox=\"0 0 48.57 48.57\" xmlns=\"http://www.w3.org/2000/svg\" role=\"img\"><title>BucketListly Logo</title><path d=\"m48.56 24.28a24.28 24.28 0 1 0 -24.28 24.29 24.28 24.28 0 0 0 24.28-24.29z\" fill=\"#eebf25\"></path><path d=\"m12.506 19.144 1.258-.871 14.231 20.567-1.258.871zm20.844 1.626-7.78 5.38-1.29-1.87-3.32 2.3-6.22-8.99 5.91-4.09 2.24 3.24 7.54-5.21.61 5.21 4.66 2.4z\" fill=\"#231f20\"></path></svg>
+                              </div>
+                            </amp-story-grid-layer>"
+                      content = ""
+                      if next_element.next_element["class"].include? "lightbox-full" or next_element.next_element["class"].include? "image-block"
+                        get_img = next_element.next_element.at_css("img")
+                        img = "<amp-story-grid-layer template=\"fill\" class=\"poster\"><amp-img animate-in=\"zoom-in\" animate-in-duration=\"30s\" src=\"#{get_img["data-original"]}\" width=\"1280\" height=\"853\" layout=\"fill\" alt=\"{{post.title}}\" srcset=\"#{get_img["data-original"]} 640w, #{get_img["data-srcset"].split(",")[0].gsub(" 500w","")} 320w\"></amp-img></amp-story-grid-layer>"
+                      end
+
+
+                      content = <<~EOS
+                      <amp-story-page id="page_#{h3_counter + 1}">
+                        #{img}#{content}#{link}                        
+                      </amp-story-page>
+                      EOS
+
+                      result << content
+                      h3_counter += 1
+                    else 
+                      result << "#{next_element.inner_html.strip}"
+                      h3_counter += 1
+                    end
+                  end
+                  next_element = next_element.next_element
+                end
+                break if h3_counter == 5;
+              end
+            end
+
+            if !result.nil?
+              return result
+            end
+          end
+        end
+
         def generate_structured_data(input)
           require 'nokogiri'
           html = Nokogiri.HTML(input)
