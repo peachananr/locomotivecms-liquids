23db127b1c083006a3b35eb4c172e3fa2eb948bf
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.41>
2024-05-25 08:56:17 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -612,13 +612,13 @@ module LocomotiveCMS
               end
             end
           end
-
-          maps_links = html.css('p > a[href*="goo.gl"]:not(.itinerary):not(.lightbox-full):not(.image-block):not(.itinerary):not(.video-block):not(.iframe-block), p > a[href*="g.page"]:not(.itinerary):not(.lightbox-full):not(.image-block):not(.itinerary):not(.video-block):not(.iframe-block), p > a[href*="google.com/maps"]:not(.itinerary):not(.lightbox-full):not(.image-block):not(.itinerary):not(.video-block):not(.iframe-block)')
-          if maps_links.size > 0
-            
-            maps_links[0]["class"] = "first-of-type #{maps_links[0]["class"]}"
-            puts "xxxxx#{maps_links[0]["class"]}"
+          html.css('p').each do |i|
+            maps_links = i.css('a[href*="goo.gl"]:not(.itinerary):not(.lightbox-full):not(.image-block):not(.itinerary):not(.video-block):not(.iframe-block), a[href*="g.page"]:not(.itinerary):not(.lightbox-full):not(.image-block):not(.itinerary):not(.video-block):not(.iframe-block), a[href*="google.com/maps"]:not(.itinerary):not(.lightbox-full):not(.image-block):not(.itinerary):not(.video-block):not(.iframe-block)')
+            if maps_links.size > 0
+              maps_links[0]["class"] = "first-of-type #{maps_links[0]["class"]}"
+            end
           end
+          
           if html.css('#pinterest').size > 0
 
             target_p = 'body > h2 ~ p:not(:empty):not(:has(img)):not(.tips-block), body > h3 ~ p:not(:empty):not(:has(img)):not(.tips-block)'
