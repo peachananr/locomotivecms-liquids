bbf384b5546a54136f805bf24b8a792406e1a8ae
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.33>
2024-05-30 20:46:16 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -197,7 +197,7 @@ module LocomotiveCMS
             end
             
           elsif html.css(".product-summary.itinerary-summary.day-to-day").size == 1
-            list = html.css(".product-summary.itinerary-summary.day-to-day .ps-row:not(:empty) > a")
+            list = html.css(".product-summary.itinerary-summary.day-to-day .ps-row:not(:empty) > a:not(.small-link)")
             list_items = ""
             list.each_with_index do |i, index| 
               l_country = location.split(",").last.strip
