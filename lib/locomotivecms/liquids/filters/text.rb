037d633619a152c2c2f25b1734d27292ea813102
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.41>
2024-05-31 10:32:33 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -63,12 +63,12 @@ module LocomotiveCMS
               #i.replace i.to_s.gsub("</p>", "</p>#{placeholder}")
             end
           end
-          if html.css(".itinerary-summary-wrapper").size > 0
-            html.at_css(".itinerary-summary-wrapper").add_previous_sibling(placeholder.gsub('content_hint', 'content_hint prerendered'))
+          #if html.css(".itinerary-summary-wrapper").size > 0
+          #  html.at_css(".itinerary-summary-wrapper").add_previous_sibling(placeholder.gsub('content_hint', 'content_hint prerendered'))
             #if html.css(".itinerary-summary-wrapper .last-minute-section").size > 0
             #  html.at_css(".itinerary-summary-wrapper .last-minute-section").add_previous_sibling(placeholder)
             #end
-          end
+          #end
           html.css(".itinerary-summary").each_with_index do |i, index|
             if (index + 1) % spread == 0
               i.replace i.to_s.gsub("</p>", "</p>#{placeholder}")
