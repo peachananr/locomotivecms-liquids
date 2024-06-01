50da5c92ebef0f188580b41ea3fecbe66b89ce8e
Pete Rojwongsuriya <peterojwongsuriya@Petes-MacBook-Pro.local>
2024-06-01 07:29:46 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -77,7 +77,7 @@ module LocomotiveCMS
               #midpoint = (items.size / 2.0).ceil
               ad_placeholder = '<div class="ads"><div class="content_hint"></div></div>'
               items[3].add_next_sibling(ad_placeholder)
-              if items.size > 19
+              if items.size > 11
                 #midpoint = (items.size / 2.0).ceil
                 ad_placeholder = '<div class="ads"><div class="content_hint"></div></div>'
                 items[8].add_next_sibling(ad_placeholder)
