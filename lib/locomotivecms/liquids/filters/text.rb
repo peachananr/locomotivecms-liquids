b88ae2bc33a50dbacbe0a2bd96257bda003b9366
Pete Rojwongsuriya <peterojwongsuriya@Petes-MacBook-Pro.local>
2024-09-05 10:10:54 +0400

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -59,8 +59,16 @@ module LocomotiveCMS
 
             if p_tag.name == "p" or p_tag.name == "ul" or p_tag.name == "ol"
               if counter == 0 
-                p_tag.add_previous_sibling("<div class=\"new-intro-open\"></div>")    
+                if p_tag.previous_element.name == "h2"
+                  p_tag.previous_element.add_previous_sibling("<div class=\"new-intro-open\"></div>")                      
+                elsif if p_tag.previous_element.name == "h3" and p_tag.previous_element["class"].include? "adj-header"
+                  if  p_tag.previous_element["class"].include? "adj-header"
+                    p_tag.previous_element.previous_element.add_previous_sibling("<div class=\"new-intro-open\"></div>")    
+                else
+                  p_tag.add_previous_sibling("<div class=\"new-intro-open\"></div>")                      
+                end
                 inside_div = true
+                
       
               elsif counter == p_limit 
                 if p_tag.text.length < 1                  
