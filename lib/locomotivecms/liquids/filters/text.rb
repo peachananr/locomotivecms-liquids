ac74583e1a6cc7c2bbc098cc54379bb223881193
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.40>
2024-09-20 17:24:13 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -124,9 +124,11 @@ module LocomotiveCMS
               end
 
               counter = counter + 1
-              # If not limit, but next element is close Block
+              # If not limit, but next element is not these, close Block
               if !p_tag.next_element.nil? and (p_tag.next_element.name == "p" or p_tag.next_element.name == "h3" or p_tag.next_element.name == "h4" or p_tag.next_element.name == "h2" or p_tag.next_element.name == "ul" or p_tag.next_element.name == "ol" or (p_tag.next_element.name == "div" and !p_tag.next_element["class"].nil? and (p_tag.next_element["class"].include? "-block" or p_tag.next_element["class"].include? "last-minute-section")))
-
+                if p_tag.next_element.next_element.nil?
+                  p_tag.next_element.add_next_sibling('<div class="new-intro-close"></div>')
+                end
               else
                 if inside_div == true
                   if !p_tag.next_element.nil?
