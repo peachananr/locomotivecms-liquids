da7af4a880ab1ed73a872e4d839a67b60ea61585
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.40>
2024-09-20 17:45:35 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -126,26 +126,30 @@ module LocomotiveCMS
               counter = counter + 1
               # If not limit, but next element is not these, close Block
               if !p_tag.next_element.nil? and (p_tag.next_element.name == "p" or p_tag.next_element.name == "h3" or p_tag.next_element.name == "h4" or p_tag.next_element.name == "h2" or p_tag.next_element.name == "ul" or p_tag.next_element.name == "ol" or (p_tag.next_element.name == "div" and !p_tag.next_element["class"].nil? and (p_tag.next_element["class"].include? "-block" or p_tag.next_element["class"].include? "last-minute-section")))
+
+
                 if p_tag.next_element.next_element.nil? and inside_div == true
-                  p_tag.next_element.add_next_sibling('<div class="new-intro-close"></div>')
+                  p_tag.next_element.add_next_sibling('<div class="new-intro-close"></div>xx')
                   counter = 0
                   inside_div = false
                   next
                 end
+
+                
               else
                 if inside_div == true
                   if !p_tag.next_element.nil?
                     if (!p_tag.next_element["class"].nil? and p_tag.next_element["class"] == "readmore-block") or p_tag.next_element.css(".accommodation-block").length > 0 or (!p_tag.next_element["class"].nil? and p_tag.next_element["class"] == "accommodation-block")                      
                       if !p_tag.next_element.next_element.nil? and (p_tag.next_element.next_element.css(".accommodation-block").length > 0 or (!p_tag.next_element.next_element["class"].nil? and p_tag.next_element.next_element["class"] == "accommodation-block"))
-                        p_tag.next_element.next_element.add_next_sibling('<div class="new-intro-close"></div>')
+                        p_tag.next_element.next_element.add_next_sibling('<div class="new-intro-close"></div>yy')
                       else
-                        p_tag.next_element.add_next_sibling('<div class="new-intro-close"></div>')
+                        p_tag.next_element.add_next_sibling('<div class="new-intro-close"></div>zz')
                       end                    
                     else
-                      p_tag.next_element.add_previous_sibling('<div class="new-intro-close"></div>')                       
+                      p_tag.next_element.add_previous_sibling('<div class="new-intro-close"></div>oo')                       
                     end
                   else
-                      p_tag.add_previous_sibling("<div class=\"new-intro-close\"></div>")                      
+                      p_tag.add_previous_sibling("<div class=\"new-intro-close\"></div>pp")                      
                   end
                   counter = 0
                   inside_div = false
