05cf08fabca42b3a8dfba67c24a00d8814e859fa
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.40>
2024-09-21 16:36:02 +0700

    New Update

--- lib/locomotivecms/liquids/filters/helpers.rb
+++ lib/locomotivecms/liquids/filters/helpers.rb
@@ -5,6 +5,15 @@ module LocomotiveCMS
         def url_for(url)
           url.chomp '/'
         end
+        def request_env (name)
+          if name.blank?
+            return ""
+          else
+            env = "#{request.env.to_h.to_yaml}" || ''
+            return env
+          end
+        end
+
         def env_variables (name)
           if name.blank?
             return ""
