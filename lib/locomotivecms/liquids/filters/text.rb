c9619353dc61d8944a6588c4d3f487d0ebc803ac
Pete Rojwongsuriya <peachananr@gmail.com>
2024-12-04 17:43:48 +0700

    New Update

--- lib/locomotivecms/liquids/filters/text.rb
+++ lib/locomotivecms/liquids/filters/text.rb
@@ -1025,16 +1025,11 @@ module LocomotiveCMS
               end
             end
 
-            if html.css('.header-placeholder.hide').size > 0 && !edit
+            if html.css('.header-placeholder.hide').size > 0
               h3_counter = 1
             
               html.css('h2').each do |h2|
-                unless h2.text.downcase.match?(/activities|more|further reading/)
-                  unless h2.text.match?(/^\d+\./)
-                    h2.inner_html = "#{h3_counter}. #{h2.inner_html.strip}"
-                    h3_counter += 1
-                  end
-                end
+               
               end                        
               html.at_css('.header-placeholder.hide').remove
             end
